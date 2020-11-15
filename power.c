8daf7154d9d4cc7d47b9276401747e979369f268
giladMoalem <giladmoa@gmail.com>
2020-11-15 20:48:54 +0200

    exponentFunctions

---
+++ power.c
@@ -0,0 +1,24 @@
+#include "myMath.h"
+#define exp 2.718281828
+
+double Pow (double x, int y){
+    double p=1;
+    if (y>0){
+    while (y>0){
+        p*=x;
+        y=y-1;
+        }
+    }
+    if (y<0){ // if y is nagative
+        y=y*-1;
+        while (y>0){
+            p=p*(1/x);
+            y=y-1;
+        } 
+    }
+    return p;
+}
+
+double Exp(int x){
+    return Pow(exp,x);
+}
